7c191f75a50d2768aab037595a1e7628c1bf0a2b
Agada Michael F <agadafidelismichael@gmail.com>
2024-10-08 10:44:32 -0700

    Add files via upload

---
+++ index..js
@@ -0,0 +1,34 @@
+//Addition //
+
+var green=15;
+var YEllow=25;
+var Blue=5;
+var color= green+ YEllow + Blue;
+document.write(color);
+document.write("<br>");
+
+
+//Subtarction//
+
+var purple=40;
+var green=20;
+var black=14;
+var color= purple- green - black;
+document.write(color);
+document.write("<br>");
+
+//Division//
+
+const book=80;
+const biro=8;
+const items= book / biro;
+document.write(items);
+document.write("<br>");
+
+//Multiplication//
+
+const sidemirrow=10;
+const wheel=10;
+const parts= sidemirrow * wheel;
+document.write(parts);
+document.write("<br>");
